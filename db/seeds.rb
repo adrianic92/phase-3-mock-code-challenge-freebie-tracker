a2cab9b0c3a3e0b54f9c31d8295686230d564fcd
Adrian <adrianic92@yahoo.com>
2023-02-27 14:08:24 -0800

    commit tree 27b22253308d0160e7972277652bf518e59080db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+Company.create(name: "Google", founding_year: 1998, id: 1)
+Company.create(name: "Facebook", founding_year: 2004, id: 2)
+Company.create(name: "Dunder Mifflin", founding_year: 2002, id: 3)
+Company.create(name: "Enron", founding_year: 1995, id: 4)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+Dev.create(name: "Rick", id: 1)
+Dev.create(name: "Morty", id: 2)
+Dev.create(name: "Mr. Meseeks", id: 3)
+Dev.create(name: "Gazorpazop", id: 4)
 
 puts "Creating freebies..."
 
@@ -17,5 +17,17 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+
+100.times do
+
+    Freebie.create(
+        item_name: Faker::Food.dish,
+        value: "#{rand(1..20)}",
+        dev_id: rand(1..4),
+        company_id: rand(1..4)
+    )
+
+end
 
 puts "Seeding done!"
